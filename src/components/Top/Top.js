ddadb84ec7e0e711c2094b1fd71dcac1abb8464a
Pratham Dogra <dogra.pratham@gmail.com>
2020-03-28 11:53:20 +0530

    Added Cards

--- src/components/Top/Top.js
+++ src/components/Top/Top.js
@@ -4,22 +4,58 @@ class Top extends Component {
   render() {
     return (
       <div className="Top">
-        <div className="flex-1">
-          <div>
-            <p>
-              The First Crypto <br />
-              Trading League
-              <br />
-              is here.
-            </p>
+        <div className="flex-container-1">
+          <div className="flex-1">
+            <div>
+              <p>
+                The First Crypto <br />
+                Trading League
+                <br />
+                is here.
+              </p>
 
-            <p id="greyText">Play. Trade. Win.</p>
+              <p id="greyText">Play. Trade. Win.</p>
+            </div>
+            <div className="box">
+              <form>
+                <input type="email" name="" placeholder="Email" />
+                <input type="submit" name="" value="Register" />
+              </form>
+            </div>
           </div>
-          <div className="box">
-            <form>
-              <input type="email" name="" placeholder="Email" />
-              <input type="submit" name="" value="Register" />
-            </form>
+          <div className="flex-2"></div>
+        </div>
+
+        <div className="flex-container-2">
+          <p>Why Participate?</p>
+        </div>
+        <div className="container">
+          <div className="card">
+            <div className="card-header">Amazing Prizes</div>
+            <div className="card-body">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-header">Amazing Prizes</div>
+            <div className="card-body">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-header">Amazing Prizes</div>
+            <div className="card-body">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </div>
           </div>
         </div>
       </div>
